d70d54ec37450fc572994e82829be136f8061b7c
berufsberatung.nlz <berufsberatung.nlz@f95.de>
2026-06-05 14:53:21 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ import StickySupport from '../components/StickySupport';
 
 const IMAGES = {
   hero: 'https://media.base44.com/images/public/6a21b8605426a2a9bf55a069/110b81c87_generated_05274896.png',
-  player: 'https://media.base44.com/images/public/6a21b8605426a2a9bf55a069/565ecf9a5_generated_498324f5.png',
+  player: 'https://media.base44.com/images/public/6a21b8605426a2a9bf55a069/fb96f9062_U19_Niederrheinpokal.jpg',
   facility: 'https://www.f95.de/media/files/fotos/news/2025-2026/August/20250815_HP_NLZ.png',
   football: 'https://media.base44.com/images/public/6a21b8605426a2a9bf55a069/76cae97f0_generated_d01aeead.png',
   team: 'https://media.base44.com/images/public/6a21b8605426a2a9bf55a069/cd04dcbf9_generated_70f9cb31.png',
